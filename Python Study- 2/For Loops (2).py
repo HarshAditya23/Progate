56443c50de42fe94f20e58698a483eb0868feba5
HarshAditya23 <34307686+HarshAditya23@users.noreply.github.com>
2020-01-20 17:25:17 +0530

    Add files via upload

---
+++ Python Study- 2/For Loops (2).py
@@ -0,0 +1,5 @@
+fruits = {'apple': 'manzana', 'orange': 'naranja', 'grape': 'uva'}
+
+# With a for loop, print '___ is ___ in Spanish'
+for fruits_name in fruits:
+    print(fruits_name + " is " + fruits[fruits_name] + " in Spanish")
